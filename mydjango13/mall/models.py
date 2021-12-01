5c0c089073c9472b254731a473733d056e56467a
kwon jeung bin <giguc1256@naver.com>
2021-12-01 13:14:43 +0900

    mall/shop telephone 추가

--- mydjango13/mall/models.py
+++ mydjango13/mall/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Shop(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=13)
+    # open_time = models.TimeField(default=datetime.time)
     photo = models.IntegerField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
